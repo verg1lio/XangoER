43fbed4632cc0d1e99a36e516898ee07a25a4bfb
Vergílio Del Claro <57490973+verg1lio@users.noreply.github.com>
2024-02-06 15:18:41 -0300

    Create digitaltwin_dynamics.py

---
+++ digitaltwin_dynamics.py
@@ -0,0 +1,50 @@
+class Dynamics:
+    """A Vehicle Dynamics object.
+
+    This class will create a Vehicle Dynamics digital twin with the springs, suspension bars, damper cylinder, brakes, tires, and transmission elements provided.
+
+    Parameters AINDA A ALTERAR, ISSO É UM EXEMPLO
+    ----------
+    shaft_elements : list
+        List with the shaft elements.
+    disk_elements : list
+        List with the disk elements.
+    bearing_elements : list
+        List with the bearing elements.
+    automeshing : boolean
+        Set it True to use the automeshing method. Default is False.
+        If automeshing is True, the previous shaft_elements parameter is now defined by:
+            shaft_elements : list
+                List with the length, inner and outter diameter and material of each element, as follows:
+                    [[length, inner, outter, material],[length, inner, outter, material],...]
+        For the other parameters please check the respective bearing and disk classes for more information.
+    **kwargs : dict, optional
+        If automeshing is True, these parameters needs to be informed.
+        The possible arguments are:
+            alpha : float
+                Proportional damping coefficient, associated to the element Mass matrix
+            beta : float
+                Proportional damping coefficient, associated to the element Stiffness matrix
+
+    Returns
+    -------
+    A rotor object.
+
+    Attributes
+    ----------
+    MM : array
+        Global mass matrix.
+    KK : array
+        Global stiffness matrix.
+    CCgyros: array
+        Global gyroscopic matrix.
+    CCtotal: array
+        Global damping matrix
+
+    Examples
+    --------
+    >>> import lmest_rotor as lm
+    >>> rotor = lm.rotor_example()
+    >>> rotor.MM
+    array(30x30)
+    """
