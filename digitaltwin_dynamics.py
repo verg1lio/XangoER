ff1f7e405e4c83c4f702b9559f868df17aede8c4
Jao-Franca <159197482+Jao-Franca@users.noreply.github.com>
2024-04-27 16:00:41 -0300

    Inits pneu digitaltwin_dynamics.py
    Adição dos inits que serão usados para modelagem do pneu posteriormente, sendo eles:
    self.tire_Fz =  carga vertical no pneu [N]
    self.tire_Sa = slip angle do pneu [rad]
    self.tire_Ls = longitudinal slip do pneu [Admensional]
    self.tire_type = tipo do modelo usado

--- digitaltwin_dynamics.py
+++ digitaltwin_dynamics.py
@@ -51,11 +51,17 @@ class Dynamics:
 
 
 
-    def __init__(self, spring_type, spring_k, spring_F):
-        self.spring_type = spring_type # Hooke, 
-        self.spring_k = spring_k # rigidez da mola [N/m]
-        self.spring_F = spring_F # força que a mola recebe [N]
-        self.spring_non_lin_coef = spring_non_lin_coef # coeficiente de ganho não-linear
+     def __init__(self, spring_type, spring_k, spring_F, spring_non_lin_coef, tire_Fz, tire_Sa, tire_Ls, tire_type):
+        # Modelo de mola
+        self.spring_type = spring_type  # Hooke, Softening
+        self.spring_k = spring_k  # rigidez da mola [N/m]
+        self.spring_F = spring_F  # força que a mola recebe [N]
+        self.spring_non_lin_coef = spring_non_lin_coef  # coeficiente de ganho não-linear
+        # Modelo de pneu
+        self.tire_Fz = tire_Fz  # carga vertical no pneu [N]
+        self.tire_Sa = tire_Sa  # slip angle do pneu [rad]
+        self.tire_Ls = tire_Ls  # longitudinal slip do pneu [Admensional]
+        self.tire_type = tire_type  # Default, Admensional
         
 
     def Spring(self):
