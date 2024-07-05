da452b099c29707ac95ae4b06fb1ee7ea3c70bf8
Nikolas Rueckmann <nkls99@gmail.com>
2024-07-05 14:07:57 -0300

    feature: Adiciona exemplo do ogata e função para plotar exemplos. Modifica nomes de variáveis e tipagens da classe

--- pwt/LagCompensator.py
+++ pwt/LagCompensator.py
@@ -1,45 +1,76 @@
-class LagCompensator:
-  def __init__(self, Kc: float, beta: float) -> None:
-    self.Kc = Kc
-    self.beta = beta
-    self.gain = Kc*beta
-    print("we on babyy!")
+import numpy as np
+import control.matlab as ml
+import matplotlib.pyplot as plt
+from typing import Callable
 
-  def Gc_of_s(self, s: float):
+class LagCompensator:
+  def __init__(self, Kc: float, beta: float, timeConstant: float) -> None:
+    self.Kc = Kc # no ideia wtf this is
+    self.ß = beta
+    self.K = Kc*beta # gain
+    self.T = timeConstant # T
+
+  def Gc_of_s(self, s: complex):
     """
     Runs a signl through the transfer function
     
     Parameters
     ----------
-    s : float
+    s : complex
       Signal input
     """
-    K = self.gain
-    T = self.T
-    beta = self.beta
 
-    return K*(T*s + 1)/(beta*T*s + 1)
+    return self.K*(self.T*s + 1)/(self.ß*self.T*s + 1)
 
-  def G_of_s(self, s: float):
+  def G_of_s(self, s: complex):
     """
     Runs a signal through the open-loop transfer function
 
     Parameters
     ----------
-    s : float
+    s : complex
       Signal input
     """
 
     return 1 / (s*(s + 1)*(0.5*s + 1))
   
-  def G1_of_s(self, s: float):
+  def G1_of_s(self, s: complex):
     """
     Runs a signl through the G1 function
     
     Parameters
     ----------
-    s : float
+    s : complex
       Signal input
     """
 
-    return self.gain * self.G_of_s(s)
+    return self.K * self.G_of_s(s)
+
+
+def ogataExample(signals: np.ndarray):
+  G = LagCompensator(1, 10, 0.2).Gc_of_s # transfer function
+
+  magnitude = 20*np.log10(np.abs(G(signals)))
+  phase = np.arctan2(np.imag(G(signals)), np.real(G(signals))) * 180/np.pi
+
+  return magnitude, phase
+
+def plotExample(example: Callable):
+  frequencyInHz = np.logspace(-4, 4, 1000)
+  frequencyInRadPerSec = 2*np.pi*frequencyInHz
+  signals = 1j*frequencyInRadPerSec
+
+  magninute, phase = example(signals)
+
+  plt.figure()
+  plt.subplot(211)
+  plt.semilogx(frequencyInRadPerSec, magninute)
+  plt.ylabel('dB')
+  plt.subplot(212)
+  plt.semilogx(frequencyInHz, phase)
+  plt.ylabel('phase')
+  plt.xlabel('rad/s')
+  plt.xlim([np.power(10.0, -4), np.power(10, 3)])
+  plt.savefig('bode-diagram.png')
+
+plotExample(ogataExample)
