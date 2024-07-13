d02863f1fa63268322a72f5b29568d703b00c907
Marco <marcoaffonso.xango@gmail.com>
2024-07-12 22:00:32 -0300

    camber_pneu.py

--- camber_pneu.py
+++ camber_pneu.py
@@ -1,51 +1,62 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Definindo uma classe chamada Dynamics
 class Dynamics:
     
-    def __init__(self, spring_type=None, spring_k=None, spring_F=None, spring_non_lin_coef=None,tire_Fz=None, tire_Sa=None, tire_Ls=None, tire_friction_coef=None, tire_Ca=None, damper_type=None, damper_V=None, damper_F_viscous=None, damper_K_friction=None, damper_F_static=None):
-        # Modelo de mola
-        self.spring_type = spring_type  # Hooke, Softening
-        self.spring_k = spring_k  # rigidez da mola [N/m]
-        self.spring_F = spring_F  # força que a mola recebe [N]
-        self.spring_non_lin_coef = spring_non_lin_coef  # coeficiente de ganho não-linear
-        # Modelo de pneu
-        self.tire_Fz = tire_Fz  # carga vertical no pneu [N]
-        self.tire_Sa = tire_Sa  # slip angle do pneu [rad]
-        self.tire_Ls = tire_Ls  # longitudinal slip do pneu [Admensional]
-        self.tire_type = 'Default'
-        self.tire_friction_coef = tire_friction_coef # coeficiente de fricção entre o pneu e a pista
-        self.tire_Ca = tire_Ca # Angulo de camber
-        # Modelo de amortecedor
-        self.damper_type = damper_type # Coulumb, Integrated, Stribeck
-        self.damper_V = damper_V # velocidade relativa amortecedor [m/s]
-        self.damper_F_viscous = damper_F_viscous # força viscosa do fluído [N]
-        self.damper_F_static = damper_F_static # coeficiente de fricção estática de coulumb [N]
-        self.damper_K_friction = damper_K_friction # rigidez de fricção [N/m]
+    def __init__(self, spring_type=None, spring_k=None, spring_F=None, spring_non_lin_coef=None, tire_Fz=None, tire_Sa=None, tire_Ls=None, tire_friction_coef=None, tire_Ca=None, damper_type=None, damper_V=None, damper_F_viscous=None, damper_K_friction=None, damper_F_static=None):
+        # Inicializando parâmetros para o modelo de mola
+        self.spring_type = spring_type  # Tipo de mola: Hooke ou Softening
+        self.spring_k = spring_k  # Rigidez da mola [N/m]
+        self.spring_F = spring_F  # Força aplicada na mola [N]
+        self.spring_non_lin_coef = spring_non_lin_coef  # Coeficiente não-linear da mola
+
+        # Inicializando parâmetros para o modelo de pneu
+        self.tire_Fz = tire_Fz  # Carga vertical no pneu [N]
+        self.tire_Sa = tire_Sa  # Ângulo de deslizamento lateral do pneu [rad]
+        self.tire_Ls = tire_Ls  # Escorregamento longitudinal do pneu [Adimensional]
+        self.tire_type = 'Default'  # Tipo de pneu
+        self.tire_friction_coef = tire_friction_coef  # Coeficiente de fricção entre pneu e pista
+        self.tire_Ca = tire_Ca  # Ângulo de camber do pneu
+
+        # Inicializando parâmetros para o modelo de amortecedor
+        self.damper_type = damper_type  # Tipo de amortecedor: Coulumb, Integrated, Stribeck
+        self.damper_V = damper_V  # Velocidade relativa do amortecedor [m/s]
+        self.damper_F_viscous = damper_F_viscous  # Força viscosa do fluído [N]
+        self.damper_F_static = damper_F_static  # Força de fricção estática de Coulumb [N]
+        self.damper_K_friction = damper_K_friction  # Rigidez de fricção [N/m]
 
     def Tire(self, params):
+        # Desembalando os parâmetros de Pacejka
         E, Cy, Cx, Cz, c1, c2 = params
+
+        # Calculando parâmetros intermediários
         Cs = c1 * np.sin(2 * np.arctan(self.tire_Fz / c2))
         D = self.tire_friction_coef * self.tire_Fz
         Bz = Cs / (Cz * D)
         Bx = Cs / (Cx * D)
         By = Cs / (Cy * D)
+
+        # Calculando a força lateral do pneu
         tire_lateral_force = D * np.sin(Cy * np.arctan(By * self.tire_Sa - E * (By * self.tire_Sa - np.arctan(By * self.tire_Sa))))
+
+        # Calculando a força longitudinal do pneu
         tire_longitudinal_force = D * np.sin(Cx * np.arctan(9 * Bx * self.tire_Ls - E * (Bx * self.tire_Ls - np.arctan(Bx * self.tire_Ls))))
+
+        # Calculando o momento de auto-alinhamento do pneu
         tire_auto_align_moment = D * np.sin(Cz * np.arctan(Bz * self.tire_Sa - E * (Bz * self.tire_Sa - np.arctan(Bz * self.tire_Sa))))
 
+        # Calculando a força de camber
         camber_thrust = D * np.sin(Cy * np.arctan(By * self.tire_Ca))
 
-
-
-        return tire_lateral_force + 0.5*camber_thrust, (10 + (tire_auto_align_moment/55)), tire_longitudinal_force
+        # Retornando as forças calculadas e o momento de auto-alinhamento
+        return tire_lateral_force + 0.5 * camber_thrust, (10 + (tire_auto_align_moment / 55)), tire_longitudinal_force
     
     def plot_camber(self, predicted_tire_lateral_forces, predicted_tire_lateral_forces_1, predicted_tire_lateral_forces_2, tire_lateral_experimental=None, tire_lateral_experimental_1=None, tire_lateral_experimental_2=None, angles=None, ratio=None):
-        
-        # Definindo um tamanho para a figura
+        # Configuração da figura e tamanho
         plt.figure(figsize=(20, 7))
 
-        # Plotagem força lateral
+        # Subplot para força lateral do pneu
         plt.subplot(1, 3, 1)
         plt.plot(self.tire_Sa, predicted_tire_lateral_forces, label='Curva com Camber')
         plt.scatter(angles, tire_lateral_experimental, color='red', label='Dados Experimentais')
@@ -55,7 +66,7 @@ class Dynamics:
         plt.legend()
         plt.grid(True)
 
-        # Plotagem torque auto-alinhante
+        # Subplot para torque auto-alinhante
         plt.subplot(1, 3, 2)
         plt.plot(self.tire_Sa, predicted_tire_lateral_forces_1, label='Curva com Camber')
         plt.scatter(angles, tire_lateral_experimental_1, color='red', label='Dados Experimentais')
@@ -65,7 +76,7 @@ class Dynamics:
         plt.legend()
         plt.grid(True)
 
-        # Plotagem força longitudinal
+        # Subplot para força longitudinal do pneu
         plt.subplot(1, 3, 3)
         plt.plot(self.tire_Sa, predicted_tire_lateral_forces_2, label='Curva com Camber')
         plt.scatter(angles, tire_lateral_experimental_2, color='red', label='Dados Experimentais')
@@ -74,12 +85,12 @@ class Dynamics:
         plt.title('Curva com Camber e Dados Experimentais')
         plt.legend()
         plt.grid(True)
+    
     def plot_graph(self, predicted_tire_lateral_forces, predicted_tire_auto_align_moment, predicted_tire_longitudinal_forces, tire_lateral_experimental=None, tire_auto_align_experimental=None, angles=None, ratio=None):
-        
-        # Definindo um tamanho para a figura
+        # Configuração da figura e tamanho
         plt.figure(figsize=(20, 7))
 
-        # Plotagem força lateral
+        # Subplot para força lateral do pneu
         plt.subplot(1, 3, 1)
         plt.plot(self.tire_Sa, predicted_tire_lateral_forces, label='Curva Otimizada')
         plt.scatter(angles, tire_lateral_experimental, color='red', label='Dados Experimentais')
@@ -89,7 +100,7 @@ class Dynamics:
         plt.legend()
         plt.grid(True)
 
-        # Plotagem torque auto-alinhante
+        # Subplot para torque auto-alinhante
         plt.subplot(1, 3, 2)
         plt.plot(self.tire_Sa, predicted_tire_auto_align_moment, label='Curva Otimizada')
         plt.scatter(angles, tire_auto_align_experimental, color='blue', label='Dados Experimentais')
@@ -99,7 +110,7 @@ class Dynamics:
         plt.legend()
         plt.grid(True)
 
-        # Plotagem força longitudinal
+        # Subplot para força longitudinal do pneu
         plt.subplot(1, 3, 3)
         plt.plot(self.tire_Ls, predicted_tire_longitudinal_forces, label='Curva Sem Otimizar')
         plt.xlabel('Taxa de Escorregamento Longitudinal (Admensional)')
@@ -108,10 +119,11 @@ class Dynamics:
         plt.legend()
         plt.grid(True)
 
-        plt.tight_layout(pad=3.0)  # Aumentando a distância entre os subplots
+        # Ajusta a disposição dos subplots para evitar sobreposição
+        plt.tight_layout(pad=3.0)
         plt.show()
 
-# Valores a serem usados na instância da classe Dynamics
+# Valores para a instância da classe Dynamics
 slip_ratio = np.linspace(-1, 1, 1000)
 slip_angles = np.linspace(-9, 9, 1000)
 
@@ -124,27 +136,28 @@ tire_auto_align_moment_1 = np.array([-28.84, -28.68, -27.21, -26.41, -27.70, -24
 camber_experimental = [-2060.0, -1950.0, -1840.0, -1700.0, -1540.0, -1350.0, -1130.0, -860.0, -480.0, -30.0, 460.0, 880.0, 1230.0, 1490.0, 1720.0, 1910.0, 2090.0, 2230.0, 2310.0]
 camber_experimental_1 = [-1940.0, -1860.0, -1750.0, -1610.0, -1450.0, -1260.0, -1050.0, -760.0, -400.0, 60.0, 540.0, 970.0, 1290.0, 1550.0, 1780.0, 1980.0, 2150.0, 2280.0, 2370.0]
 camber_experimental_15 = [-1840.0, -1750.0, -1670.0, -1520.0, -1370.0, -1180.0, -960.0, -680.0, -310.0, 130.0, 610.0, 1020.0, 1360.0, 1630.0, 1850.0, 2040.0, 2220.0, 2360.0, 2430.0]
-# Parâmetros de Pacejka 
+
+# Parâmetros de Pacejka
 result = [(0.3336564873588197), (1.6271741344929977), (1), (4.3961693695846655), (931.4055775279057), (366.4936818126405)]
 
-# Instanciando a classe Dynamics
+# Instanciando a classe Dynamics sem camber
 dynamics_instance = Dynamics(tire_Fz=1500, tire_Sa=slip_angles, tire_Ls=slip_ratio, tire_friction_coef=1.45, tire_Ca=0)
 
+# Calculando as forças previstas e o momento de auto-alinhamento
 predicted_tire_lateral_forces, predicted_tire_auto_align_moment, predicted_tire_longitudinal_forces = dynamics_instance.Tire(result)
 
+# Instanciando a classe Dynamics com diferentes valores de camber
 dynamics_camber = Dynamics(tire_Fz=1500, tire_Sa=slip_angles, tire_Ls=slip_ratio, tire_friction_coef=1.45, tire_Ca=0.5)
 dynamics_camber_1 = Dynamics(tire_Fz=1500, tire_Sa=slip_angles, tire_Ls=slip_ratio, tire_friction_coef=1.45, tire_Ca=1)
 dynamics_camber_15 = Dynamics(tire_Fz=1500, tire_Sa=slip_angles, tire_Ls=slip_ratio, tire_friction_coef=1.45, tire_Ca=1.5)
 
+# Calculando as forças laterais previstas para diferentes valores de camber
 forca_camber = dynamics_camber.Tire(result)[0]
-forca1_camber_1= dynamics_camber_1.Tire(result)[0]
+forca1_camber_1 = dynamics_camber_1.Tire(result)[0]
 forca2_camber_15 = dynamics_camber_15.Tire(result)[0]
 
+# Plotando as curvas com camber comparadas com dados experimentais
 dynamics_camber.plot_camber(forca_camber, forca1_camber_1, forca2_camber_15, camber_experimental, camber_experimental_1, camber_experimental_15, angles)
 
+# Plotando as curvas previstas com os dados experimentais
 dynamics_instance.plot_graph(predicted_tire_lateral_forces, predicted_tire_auto_align_moment, predicted_tire_longitudinal_forces)
-
-
-
-
-
