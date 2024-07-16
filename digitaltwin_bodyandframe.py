cb9d8a01f915450e4c9e25d71d8e17ac9d760ed8
Patr1cia <161897113+Patr1cia@users.noreply.github.com>
2024-07-15 23:46:53 -0300

    Update digitaltwin_bodyandframe.py

--- digitaltwin_bodyandframe.py
+++ digitaltwin_bodyandframe.py
@@ -53,28 +53,55 @@ class BodyAndFrame:
 
 
     def __init__(self, nos, massa, momento_inercia_direção, momento_inercia_plano):
-        self.nos = nos #Number of chassi's nodes
-        self.massa = massa #Car mass (Kg)
-        self.momento_inercia_direção = momento_inercia_direção #Moment of inertia relative to direction (Kg.m^2)
-        self.momento_inercia_plano = momento_inercia_plano #Moment of inertia relative to the plane (Kg.m^2)
+        self.I = I #Tirar I e E e colocar em elemento de Barra e viga numa nova def
+        self.E = E
+        self.num_nodes = n  # Número total de nós
+        self.massa = m  # Massa do carro (Kg)
+        self.momento_inercia_direcao = iD  # Momento de inércia em relação à direção (Kg.m^2)
+        self.momento_inercia_plano = IP  # Momento de inércia em relação ao plano (Kg.m^2)
+        self.area_transversal = area_transversal  # Área da seção transversal da Estrutura
+        self.num_dofs_per_node = 6  # Graus de liberdade por nó
+        self.num_dofs = self.num_nodes * self.num_dofs_per_node  # Total de Graus de liberdade (gdls)
+        self.K_global = np.zeros((self.num_dofs, self.num_dofs))  # Matriz de rigidez global
+        
         
 
     def Connect(self):
-        """Description.
+         
+         def node_loc_matrix(self, node_tags, node_coord):
+        # Número de Nós
+        num_nodes = len(node_tags)
 
-        Detailed description.
+        # Gerando uma matrix número de nos x 4: (Para cada linha, teremos: [índice do nó, x, y, z])
+        node_loc_matrix = np.zeros((num_nodes, 4),
+                                   dtype=float)  # Na primeira coluna, teremos os índices, nas seguintes, x y e z.
 
-        Returns
-        -------
-        Bat : variable type
-            Description.
+        # Preenchendo a matriz de zeros
+        for i, (x, y, z) in enumerate(node_coord, start=0):
+            node_loc_matrix[i][0] = node_tags[i]  # Número do nó na primeira coluna
+            node_loc_matrix[i][1] = x  # Coordenada x na segunda coluna
+            node_loc_matrix[i][2] = y  # Coordenada y na terceira coluna
+            node_loc_matrix[i][3] = z  # Coordenada z na quarta coluna
 
-        Examples
-        --------
-        >>> example
-        """
-        
-        return Con
+        print("\n   Nó   x   y   z")
+        print(node_loc_matrix)
+
+    def connect_matrix(self):
+        # Número de conexões
+        num_connect = len(self.elements)
+
+        # Gerando a matriz de conectividade: (Para cada linha, teremos: [índice a coneção, 1º nó, 2º nó])
+        CM = np.zeros((num_connect, 3), dtype=int)
+
+        # Preenchendo a matriz:
+        for i, (no1, no2) in enumerate(self.elements, start=0):
+            CM[i][0] = i + 1
+            CM[i][1] = no1
+            CM[i][2] = no2
+
+        print("\n Conexão   1º Nó   2º Nó")
+        print(CM)
+
 
 
 
