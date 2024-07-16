fd50e71a5990f43ae803bbbfb5ba8bb825447e36
Marco <marcoaffonso.xango@gmail.com>
2024-07-15 22:28:48 -0300

    Create camber_pneu_v1.py

---
+++ camber_pneu_v1.py
@@ -0,0 +1,144 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Definindo uma classe chamada Dynamics
+class Dynamics:
+        # Inicializando parâmetros para o modelo de mola
+    def __init__(self, spring_type=None, spring_k=None, spring_F=None, spring_non_lin_coef=None, tire_Fz=None, tire_Sa=None, tire_Ls=None, tire_friction_coef=None, tire_Ca=None, damper_type=None, damper_V=None, damper_F_viscous=None, damper_K_friction=None, damper_F_static=None):
+        
+        self.spring_type = spring_type  # Tipo de mola: Hooke ou Softening
+        self.spring_k = spring_k  # Rigidez da mola [N/m]
+        self.spring_F = spring_F  # Força aplicada na mola [N]
+        self.spring_non_lin_coef = spring_non_lin_coef  # Coeficiente não-linear da mola
+
+        # Inicializando parâmetros para o modelo de pneu
+        self.tire_Fz = tire_Fz  # Carga vertical no pneu [N]
+        self.tire_Sa = tire_Sa  # Ângulo de deslizamento lateral do pneu [rad]
+        self.tire_Ls = tire_Ls  # Escorregamento longitudinal do pneu [Adimensional]
+        self.tire_type = 'Default'  # Tipo de pneu
+        self.tire_friction_coef = tire_friction_coef  # Coeficiente de fricção entre pneu e pista
+        self.tire_Ca = tire_Ca  # Ângulo de camber do pneu
+
+        # Inicializando parâmetros para o modelo de amortecedor
+        self.damper_type = damper_type  # Tipo de amortecedor: Coulumb, Integrated, Stribeck
+        self.damper_V = damper_V  # Velocidade relativa do amortecedor [m/s]
+        self.damper_F_viscous = damper_F_viscous  # Força viscosa do fluído [N]
+        self.damper_F_static = damper_F_static  # Força de fricção estática de Coulumb [N]
+        self.damper_K_friction = damper_K_friction  # Rigidez de fricção [N/m]
+      
+        # Calcula as diferentes forças e influências sobre o sistema
+    def Tire(self, params):
+        # Desembalando os parâmetros de Pacejka
+        E, Cy, Cx, Cz, c1, c2 = params
+
+        # Calculando parâmetros intermediários
+        Cs = c1 * np.sin(2 * np.arctan(self.tire_Fz / c2))
+        D = self.tire_friction_coef * self.tire_Fz
+        Bz = Cs / (Cz * D)
+        Bx = Cs / (Cx * D)
+        By = Cs / (Cy * D)
+
+        # Calculando a força lateral do pneu
+        tire_lateral_force = D * np.sin(Cy * np.arctan(By * self.tire_Sa - E * (By * self.tire_Sa - np.arctan(By * self.tire_Sa))))
+
+        # Calculando a força longitudinal do pneu
+        tire_longitudinal_force = D * np.sin(Cx * np.arctan(9 * Bx * self.tire_Ls - E * (Bx * self.tire_Ls - np.arctan(Bx * self.tire_Ls))))
+
+        # Calculando o momento de auto-alinhamento do pneu
+        tire_auto_align_moment = D * np.sin(Cz * np.arctan(Bz * self.tire_Sa - E * (Bz * self.tire_Sa - np.arctan(Bz * self.tire_Sa))))
+
+        # Calculando a força de camber
+        camber_thrust = D * np.sin(Cy * np.arctan(By * self.tire_Ca))
+
+        # Retornando as forças calculadas e o momento de auto-alinhamento
+        return tire_lateral_force + 0.5 * camber_thrust, (10 + (tire_auto_align_moment / 55)), tire_longitudinal_force
+      
+        # Plota os graficos respectivos a 3 valores diferentes de camber
+    def plot_camber(self, predicted_tire_lateral_forces, predicted_tire_lateral_forces_1, predicted_tire_lateral_forces_2, tire_lateral_experimental=None, tire_lateral_experimental_1=None, tire_lateral_experimental_2=None, angles=None, ratio=None):
+        # Configuração da figura e tamanho
+        plt.figure(figsize=(20, 7))
+
+        # Subplot para força lateral do pneu
+        plt.subplot(1, 3, 1)
+        plt.plot(self.tire_Sa, predicted_tire_lateral_forces, label='Curva com Camber')
+        plt.scatter(angles, tire_lateral_experimental, color='red', label='Dados Experimentais')
+        plt.xlabel('Ângulo de Deslizamento Lateral (graus)')
+        plt.ylabel('Força Lateral do Pneu (N)')
+        plt.title('Curva com Camber e Dados Experimentais')
+        plt.legend()
+        plt.grid(True)
+
+        # Subplot para torque auto-alinhante
+        plt.subplot(1, 3, 2)
+        plt.plot(self.tire_Sa, predicted_tire_lateral_forces_1, label='Curva com Camber')
+        plt.scatter(angles, tire_lateral_experimental_1, color='red', label='Dados Experimentais')
+        plt.xlabel('Ângulo de Deslizamento Lateral (graus)')
+        plt.ylabel('Força Lateral do Pneu (N)')
+        plt.title('Curva com Camber e Dados Experimentais')
+        plt.legend()
+        plt.grid(True)
+
+        # Subplot para força longitudinal do pneu
+        plt.subplot(1, 3, 3)
+        plt.plot(self.tire_Sa, predicted_tire_lateral_forces_2, label='Curva com Camber')
+        plt.scatter(angles, tire_lateral_experimental_2, color='red', label='Dados Experimentais')
+        plt.xlabel('Ângulo de Deslizamento Lateral (graus)')
+        plt.ylabel('Força Lateral do Pneu (N)')
+        plt.title('Curva com Camber e Dados Experimentais')
+        plt.legend()
+        plt.grid(True)
+      
+        # Plota os graficos com caber 0   
+    def plot_graph(self, predicted_tire_lateral_forces, predicted_tire_auto_align_moment, predicted_tire_longitudinal_forces, tire_lateral_experimental=None, tire_auto_align_experimental=None, angles=None, ratio=None):
+        # Configuração da figura e tamanho
+        plt.figure(figsize=(20, 7))
+
+        # Subplot para força lateral do pneu
+        plt.subplot(1, 3, 1)
+        plt.plot(self.tire_Sa, predicted_tire_lateral_forces, label='Curva Otimizada')
+        plt.scatter(angles, tire_lateral_experimental, color='red', label='Dados Experimentais')
+        plt.xlabel('Ângulo de Deslizamento Lateral (graus)')
+        plt.ylabel('Força Lateral do Pneu (N)')
+        plt.title('Curva Otimizada com os Dados Experimentais')
+        plt.legend()
+        plt.grid(True)
+
+        # Subplot para torque auto-alinhante
+        plt.subplot(1, 3, 2)
+        plt.plot(self.tire_Sa, predicted_tire_auto_align_moment, label='Curva Otimizada')
+        plt.scatter(angles, tire_auto_align_experimental, color='blue', label='Dados Experimentais')
+        plt.xlabel('Ângulo de Deslizamento Lateral (graus)')
+        plt.ylabel('Torque auto-alinhante (N.m)')
+        plt.title('Curva Otimizada com os Dados Experimentais')
+        plt.legend()
+        plt.grid(True)
+
+        # Subplot para força longitudinal do pneu
+        plt.subplot(1, 3, 3)
+        plt.plot(self.tire_Ls, predicted_tire_longitudinal_forces, label='Curva Sem Otimizar')
+        plt.xlabel('Taxa de Escorregamento Longitudinal (Admensional)')
+        plt.ylabel('Força Longitudinal (N)')
+        plt.title('Força Longitudinal - Sem Dados Experimentais')
+        plt.legend()
+        plt.grid(True)
+
+        # Ajusta a disposição dos subplots para evitar sobreposição
+        plt.tight_layout(pad=3.0)
+        plt.show()
+
+# Criação de arrays com valores para plotagem e análise
+slip_ratio = np.linspace(-1, 1, 1000)
+slip_angles = np.linspace(-9, 9, 1000)
+
+# Dados experimentais
+ratio = np.linspace(-1, 1, 19)
+angles = np.array([-9.0, -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0])
+tire_lateral_forces_1 = np.array([-2300, -2200, -2060, -1880, -1680, -1450, -1190, -850, -430, 60, 520, 890, 1170, 1390, 1580, 1730, 1890, 2000, 2090])
+tire_auto_align_moment_1 = np.array([-28.84, -28.68, -27.21, -26.41, -27.70, -24.21, -24.15, -15.88, -4.91, 14.72, 33.80, 43.79, 46.93, 49.09, 50.90, 50.10, 50.81, 48.12, 48.83])
+
+camber_experimental = [-2060.0, -1950.0, -1840.0, -1700.0, -1540.0, -1350.0, -1130.0, -860.0, -480.0, -30.0, 460.0, 880.0, 1230.0, 1490.0, 1720.0, 1910.0, 2090.0, 2230.0, 2310.0]
+camber_experimental_1 = [-1940.0, -1860.0, -1750.0, -1610.0, -1450.0, -1260.0, -1050.0, -760.0, -400.0, 60.0, 540.0, 970.0, 1290.0, 1550.0, 1780.0, 1980.0, 2150.0, 2280.0, 2370.0]
+camber_experimental_15 = [-1840.0, -1750.0, -1670.0, -1520.0, -1370.0, -1180.0, -960.0, -680.0, -310.0, 130.0, 610.0, 1020.0, 1360.0, 1630.0, 1850.0, 2040.0, 2220.0, 2360.0, 2430.0]
+
+# Parâmetros de Pacejka
+result = [(0.3336564873588197), (1.6271741344929977), (1), (4.3961693695846655), (931.4055775279057), (366.4936818126405)]
