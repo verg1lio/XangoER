5dd3877068451d1fb8b778f2d8837637d5b272a6
RuanLutigards <ruanlutigards.xango@gmail.com>
2024-07-16 21:19:25 -0300

    Update V_1.0_freio.py

--- V_1.0_freio.py
+++ V_1.0_freio.py
@@ -1,117 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
-Classe que encapsula modelos e métodos para simular o comportamento dinâmico de um sistema veicular,
-especificamente focando em molas, pneus e amortecedores.
-'''
-class Dynamics:
-
-    # Função para definir todos os parâmetros necessários
-    def __init__(self, spring_type=None, spring_k=None, spring_F=None, spring_non_lin_coef=None, tire_Fz=None,
-               tire_Sa=None, tire_Ls=None, tire_friction_coef=None, damper_type=None, damper_V=None,
-               damper_F_viscous=None, damper_K_friction=None, damper_F_static=None):
-        # Inicializa o modelo de mola
-        self.spring_type = spring_type  # Hooke, Softening
-        self.spring_k = spring_k  # rigidez da mola [N/m]
-        self.spring_F = spring_F  # força que a mola recebe [N]
-        self.spring_non_lin_coef = spring_non_lin_coef  # coeficiente de ganho não-linear
-        # Inicializa o modelo de pneu
-        self.tire_Fz = tire_Fz  # carga vertical no pneu [N]
-        self.tire_Sa = tire_Sa  # slip angle do pneu [rad]
-        self.tire_Ls = tire_Ls  # longitudinal slip do pneu [Admensional]
-        self.tire_type = 'Default'
-        self.tire_friction_coef = tire_friction_coef  # coeficiente de fricção entre o pneu e a pista
-        # Inicializa o modelo de amortecedor
-        self.damper_type = damper_type  # Coulumb, Integrated, Stribeck
-        self.damper_V = damper_V  # velocidade relativa amortecedor [m/s]
-        self.damper_F_viscous = damper_F_viscous  # força viscosa do fluído [N]
-        self.damper_F_static = damper_F_static  # coeficiente de fricção estática de coulumb [N]
-        self.damper_K_friction = damper_K_friction  # rigidez de fricção [N/m]
-
-    # Função que calcula as forças no pneu (lateral, longitudinal e torque auto-alinhante)
-    def Tire(self, params):
-        E, Cy, Cx, Cz, c1, c2 = params
-        Cs = c1 * np.sin(2 * np.arctan(self.tire_Fz / c2))
-        D = self.tire_friction_coef * self.tire_Fz
-        Bz = Cs / (Cz * D)
-        Bx = Cs / (Cx * D)
-        By = Cs / (Cy * D)
-        tire_lateral_force = D * np.sin(
-            Cy * np.arctan(By * self.tire_Sa - E * (By * self.tire_Sa - np.arctan(By * self.tire_Sa))))
-        tire_longitudinal_force = D * np.sin(
-            Cx * np.arctan(9 * Bx * self.tire_Ls - E * (Bx * self.tire_Ls - np.arctan(Bx * self.tire_Ls))))
-        tire_auto_align_moment = D * np.sin(
-            Cz * np.arctan(Bz * self.tire_Sa - E * (Bz * self.tire_Sa - np.arctan(Bz * self.tire_Sa))))
-
-        return tire_lateral_force, (10 + (tire_auto_align_moment / 55)), tire_longitudinal_force
-
-    # Função que calcula a razão de escorregamento (slip ratio) com base na velocidade angular e no raio do pneu
-    def slip_ratio_1(self, velocidade_angular, raio_pneu):
-        velocidade_linear = initial_speed
-        value = (velocidade_angular * raio_pneu / velocidade_linear) - 1
-
-        return value
-
-    # Função que exibe os valores do slip ratio
-    def show_results(self, value):
-        print("Valores do Slip Ratio: ")
-
-        for dado in value:
-            print(dado)
-
-    # Função que plota o gráfico do slip ratio em relação à força do pedal
-    def plot_graph_slip_ratio(self, value=None):
-        plt.figure(figsize=(10, 6))
-        plt.plot(pedal_forces, value, label="Slip Ratio vs. Força do Pedal")
-
-        # Invertendo o eixo y
-        plt.gca().invert_yaxis()
-        plt.xlabel("Força no Pedal (N)")
-        plt.ylabel("Slip Ratio")
-        plt.title("Força no Pedal em Relação ao Slip Ratio de Frenagem")
-        plt.grid(True)
-        plt.legend()
-        plt.show()
-        
-    # Função que plota os gráficos das forças laterais, longitudinais e torque auto-alinhante do pneu
-    def plot_graph(self, predicted_tire_lateral_forces, predicted_tire_auto_align_moment,
-                   predicted_tire_longitudinal_forces, tire_lateral_experimental=None,
-                   tire_auto_align_experimental=None, angles=None, ratio=None, pedal_forces=None, value=None):
-        plt.figure(figsize=(20, 7))
-
-        # Plotagem força lateral
-        plt.subplot(1, 3, 1)
-        plt.plot(self.tire_Sa, predicted_tire_lateral_forces, label='Curva Otimizada')
-        plt.scatter(angles, tire_lateral_experimental, color='red', label='Dados Experimentais')
-        plt.xlabel('Ângulo de Deslizamento Lateral (graus)')
-        plt.ylabel('Força Lateral do Pneu (N)')
-        plt.title('Curva Otimizada com os Dados Experimentais')
-        plt.legend()
-        plt.grid(True)
-
-        # Plotagem torque auto-alinhante
-        plt.subplot(1, 3, 2)
-        plt.plot(self.tire_Sa, predicted_tire_auto_align_moment, label='Curva Otimizada')
-        plt.scatter(angles, tire_auto_align_experimental, color='blue', label='Dados Experimentais')
-        plt.xlabel('Ângulo de Deslizamento Lateral (graus)')
-        plt.ylabel('Torque auto-alinhante (N.m)')
-        plt.title('Curva Otimizada com os Dados Experimentais')
-        plt.legend()
-        plt.grid(True)
-
-        # Plotagem força longitudinal
-        plt.subplot(1, 3, 3)
-        plt.plot(self.tire_Ls, predicted_tire_longitudinal_forces, label='Curva Sem Otimizar')
-        plt.xlabel('Taxa de Escorregamento Longitudinal (Admensional)')
-        plt.ylabel('Força Longitudinal (N)')
-        plt.title('Força Longitudinal - Sem Dados Experimentais')
-        plt.legend()
-        plt.grid(True)
-
-        plt.tight_layout(pad=3.0)  # Aumentando a distância entre os subplots
-        plt.show()
-
 
 '''
 Classe que modela o sistema de frenagem de um veículo e calcula diversos parâmetros relacionados à frenagem, 
@@ -291,16 +180,7 @@ params = {
 initial_speed = 20  # Velocidade inicial do veículo [km/h]
 tempo = 2  # Tempo para o cálculo [s]
 
-# Parâmetros de Pacejka
-result = [(0.3336564873588197), (1.6271741344929977), (1), (4.3961693695846655), (931.4055775279057), (366.4936818126405)]
-
 # Criação de arrays com valores para plotagem e análise
-slip_ratio = np.linspace(-1, 1, 1000)  # Razão de escorregamento variando de -1 a 1
-slip_angles = np.linspace(-9, 9, 1000)  # Ângulos de deslizamento variando de -9 a 9 graus
 pedal_forces = np.linspace(0, 823, 1000)  # Forças do pedal variando de 0 a 823 N
 
-# Dados experimentais dos ângulos de deslizamento e forças do pneu
-angles = np.array([-9.0, -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0])
-tire_lateral_forces_1 = np.array([-2300, -2200, -2060, -1880, -1680, -1450, -1190, -850, -430, 60, 520, 890, 1170, 1390, 1580, 1730, 1890, 2000, 2090])
-tire_auto_align_moment_1 = np.array([-28.84, -28.68, -27.21, -26.41, -27.70, -24.21, -24.15, -15.88, -4.91, 14.72, 33.80, 43.79, 46.93, 49.09, 50.90, 50.10, 50.81, 48.12, 48.83])
 
