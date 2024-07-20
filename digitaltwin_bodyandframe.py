524561d173525ff9e5d1f0a8a8855e7cafbd8586
Patr1cia <161897113+Patr1cia@users.noreply.github.com>
2024-07-19 23:10:24 -0300

    Update digitaltwin_bodyandframe.py

--- digitaltwin_bodyandframe.py
+++ digitaltwin_bodyandframe.py
@@ -1,254 +1,316 @@
-class BodyAndFrame:
-    """A Body And Frame object.
-
-    This class will create a Body And Frame digital twin with the chassis elements provided.
-
-    Parameters AINDA A ALTERAR, ISSO É UM EXEMPLO
-    ----------
-    shaft_elements : list
-        List with the shaft elements.
-    disk_elements : list
-        List with the disk elements.
-    bearing_elements : list
-        List with the bearing elements.
-    automeshing : boolean
-        Set it True to use the automeshing method. Default is False.
-        If automeshing is True, the previous shaft_elements parameter is now defined by:
-            shaft_elements : list
-                List with the length, inner and outter diameter and material of each element, as follows:
-                    [[length, inner, outter, material],[length, inner, outter, material],...]
-        For the other parameters please check the respective bearing and disk classes for more information.
-    **kwargs : dict, optional
-        If automeshing is True, these parameters needs to be informed.
-        The possible arguments are:
-            alpha : float
-                Proportional damping coefficient, associated to the element Mass matrix
-            beta : float
-                Proportional damping coefficient, associated to the element Stiffness matrix
-
-    Returns
-    -------
-    A rotor object.
-
-    Attributes
-    ----------
-    MM : array
-        Global mass matrix.
-    KK : array
-        Global stiffness matrix.
-    CCgyros: array
-        Global gyroscopic matrix.
-    CCtotal: array
-        Global damping matrix
-
-    Examples
-    --------
-    >>> import lmest_rotor as lm
-    >>> rotor = lm.rotor_example()
-    >>> rotor.MM
-    array(30x30)
-    """
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
+class Estrutura:
 
+    def __init__(self, elements, nodes, m, Id, Ip):
+        self.elements = elements                                              # Matriz de elementos conectados
+        self.num_elements = len(elements)                                     # Número de elementos
+        self.nodes = nodes                                                    # Matriz de nós com suas posições
+        self.num_nodes = len(nodes)                                           # Número total de nós
+        self.massa = m                                                        # Massa do carro (Kg)
+        self.momento_inercia_direcao = Id                                     # Momento de inércia em relação à direção (kg.m^2)
+        self.momento_inercia_plano = Ip                                       # Momento de inércia em relação ao plano (kg.m^2)
+        self.num_dofs_per_node = 6                                            # Graus de liberdade por nó
+        self.num_dofs = self.num_nodes * self.num_dofs_per_node               # Total de Graus de liberdade (gdls)
+        self.K_global = np.zeros((self.num_dofs, self.num_dofs))              # Matriz de rigidez global
+        self.M_global = np.zeros((self.num_dofs, self.num_dofs))              # Matriz de massa global
+        self.num_modes = 12                                                   # Número de modos de vibração a serem retornados
 
-    def __init__(self, nos, massa, momento_inercia_direção, momento_inercia_plano):
-        self.I = I #Tirar I e E e colocar em elemento de Barra e viga numa nova def
-        self.E = E
-        self.num_nodes = n  # Número total de nós
-        self.massa = m  # Massa do carro (Kg)
-        self.momento_inercia_direcao = iD  # Momento de inércia em relação à direção (Kg.m^2)
-        self.momento_inercia_plano = IP  # Momento de inércia em relação ao plano (Kg.m^2)
-        self.area_transversal = area_transversal  # Área da seção transversal da Estrutura
-        self.num_dofs_per_node = 6  # Graus de liberdade por nó
-        self.num_dofs = self.num_nodes * self.num_dofs_per_node  # Total de Graus de liberdade (gdls)
-        self.K_global = np.zeros((self.num_dofs, self.num_dofs))  # Matriz de rigidez global
-        
+    def calcular_comprimento(self, element):
+        node1, node2 = element
+        x1, y1, z1 = self.nodes[node1]
+        x2, y2, z2 = self.nodes[node2]
+        return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
         
 
-    def Connect(self):
-         
-         def node_loc_matrix(self, node_tags, node_coord):
-        # Número de Nós
+    def node_loc_matrix(self, node_tags, node_coord):
         num_nodes = len(node_tags)
-
-        # Gerando uma matrix número de nos x 4: (Para cada linha, teremos: [índice do nó, x, y, z])
-        node_loc_matrix = np.zeros((num_nodes, 4),
-                                   dtype=float)  # Na primeira coluna, teremos os índices, nas seguintes, x y e z.
-
-        # Preenchendo a matriz de zeros
+        node_loc_matrix = np.zeros((num_nodes, 4), dtype=float)
         for i, (x, y, z) in enumerate(node_coord, start=0):
-            node_loc_matrix[i][0] = node_tags[i]  # Número do nó na primeira coluna
-            node_loc_matrix[i][1] = x  # Coordenada x na segunda coluna
-            node_loc_matrix[i][2] = y  # Coordenada y na terceira coluna
-            node_loc_matrix[i][3] = z  # Coordenada z na quarta coluna
-
+            node_loc_matrix[i][0] = node_tags[i] + 1
+            node_loc_matrix[i][1] = x
+            node_loc_matrix[i][2] = y
+            node_loc_matrix[i][3] = z
         print("\n   Nó   x   y   z")
         print(node_loc_matrix)
 
     def connect_matrix(self):
-        # Número de conexões
-        num_connect = len(self.elements)
-
-        # Gerando a matriz de conectividade: (Para cada linha, teremos: [índice a coneção, 1º nó, 2º nó])
+        num_connect = len(self.elements)                                     # Número de conexões
         CM = np.zeros((num_connect, 3), dtype=int)
-
-        # Preenchendo a matriz:
         for i, (no1, no2) in enumerate(self.elements, start=0):
             CM[i][0] = i + 1
             CM[i][1] = no1
             CM[i][2] = no2
-
         print("\n Conexão   1º Nó   2º Nó")
         print(CM)
 
+    def matrizes_elementares(self, element):
+        E = 210e9  # Pa
+        I = 1.6667e-5  # m^4
+        rho = 7850  # kg/m^3
+        A = 0.0125  # m^2
 
+        L_e = self.calcular_comprimento(element)
+        self.k_e = (E * I / L_e ** 3) * np.array([
+            [12, 6 * L_e, -12, 6 * L_e],
+            [6 * L_e, 4 * L_e ** 2, -6 * L_e, 2 * L_e ** 2],
+            [-12, -6 * L_e, 12, -6 * L_e],
+            [6 * L_e, 2 * L_e ** 2, -6 * L_e, 4 * L_e ** 2]
+        ])
 
+        self.m_e = (rho * A * L_e / 420) * np.array([
+            [156, 22 * L_e, 54, -13 * L_e],
+            [22 * L_e, 4 * L_e ** 2, 13 * L_e, -3 * L_e ** 2],
+            [54, 13 * L_e, 156, -22 * L_e],
+            [-13 * L_e, -3 * L_e ** 2, -22 * L_e, 4 * L_e ** 2]
+        ])
 
-    def ElementMat(self):
-        """Description.
+        return self.k_e, self.m_e
 
-        Detailed description.
+    def matrizes_globais(self):
+        for element in self.elements:
+            node1, node2 = element
+            self.k_e, self.m_e = self.matrizes_elementares(element)
 
-        Returns
-        -------
-        Bat : variable type
-            Description.
+            dofs = [2 * node1, 2 * node1 + 1, 2 * node2, 2 * node2 + 1]
+            for i in range(4):
+                for j in range(4):
+                    self.K_global[dofs[i], dofs[j]] += self.k_e[i, j]
+                    self.M_global[dofs[i], dofs[j]] += self.m_e[i, j]
 
-        Examples
-        --------
-        >>> example
-        """
-        
-        return Ele
+        # Ancoragem
+        ancoragem_nos = [0, 2, 4, 5]
+        for no in ancoragem_nos:
+            for i in range(self.num_dofs_per_node):
+                idx = no * self.num_dofs_per_node + i
+                self.K_global[idx, idx] = 1e20                          #Matriz de Rigidez GLO
+                self.M_global[idx, idx] = 1e20                          #Matriz de Massa GLO
 
+        return self.K_global, self.M_global
 
+    def shape_fun(self, F1,F2,F3):
+        E = 210e9   # Pa                                                #Módulo de Elasticidade
+        I = 1.6667e-5 # m^4                                             #Momento de Inercia  
+        G = 81.2e9  # Pa                                                #Módulo de Elasticidade em Cisalhamento
+        A= 0.0125                                                       #Área da seção Transversal
+        J = 1e-6    # m^4 (momento polar de inércia)
+        torsao, deformacao, flexao = [], [], []
+        for element in self.elements:
+            L_e = self.calcular_comprimento(element)
+            # Equação de torção
+            torsao_val = (F1 * L_e) / (G * J)
+            torsao.append(torsao_val)
+            # Equação diferencial para deformação axial
+            deformacao_val = (F2 / (A * E)) * L_e
+            deformacao.append(deformacao_val)
+            # Equação de Euler-Bernoulli para flexão
+            x = np.linspace(0, L_e, len(F3))
+            v_val = np.zeros_like(F3)
+            for i in range(1, len(F3)):
+                v_val[i] = (F3[i] - F3[i-1]) / (E * I) * (L_e ** 3 / 12)
+            flexao.append(v_val)
 
-    def GlobalMat(self):
-        """Description.
-
-        Detailed description.
-
-        Returns
-        -------
-        Bat : variable type
-            Description.
-
-        Examples
-        --------
-        >>> example
-        """
-        
-        return Glo
-
+        return np.array(torsao), np.array(deformacao), np.array(flexao)
     
+    def modal_analysis(self):
+        # Análise modal por resolução do problema de autovalor e autovetor
+        unsorted_eigenvalues, unsorted_eigenvectors = eigh(self.K_global, self.M_global)
 
-    def ShapeFun(self):
-        """Description.
+        # Frequências naturais (raiz quadrada dos autovalores)
+        unsorted_frequencies = np.sqrt(unsorted_eigenvalues) / (2 * np.pi)  # Divisão por 2*pi para converter para hertz
 
-        Detailed description.
+        # Tratando os dados (tomando apenas as 20 primeiras frequências naturais)
+        sorted_indices = np.argsort(unsorted_frequencies)  # Ordena as frequências em ordem crescente
+        top_indices = sorted_indices[:self.num_modes]  # Seleciona os índices dos primeiros n modos
 
-        Returns
-        -------
-        Bat : variable type
-            Description.
+        eigenvalues = np.array(unsorted_eigenvalues)[top_indices]  # Filtra os primeiros n autovalores
+        eigenvectors = np.array(unsorted_eigenvectors)[:, top_indices]  # Filtra os primeiros n autovetores
+        frequencies = np.array(unsorted_frequencies)[top_indices]  # Filtra as primeiras n frequências
 
-        Examples
-        --------
-        >>> example
-        """
-        
-        return SF
+        return eigenvalues, eigenvectors, frequencies
 
+    def Mesh(self):
 
+        filename = input("Insira o nome do arquivo: ") + ".geo"
+        diretorio = input("Insira o diretorio onde o arquivo .geo deve ser salvo: ")
 
-    def WeakForm(self):
-        """Description.
-
-        Detailed description.
-
-        Returns
-        -------
-        Bat : variable type
-            Description.
-
-        Examples
-        --------
-        >>> example
-        """
-        
-        return WF
-
-
-
-    def Mesh(self, Coordenadas_nos, conexões_nos):
-        
-        filename=input("Insira o nome do arquivo: ") +".geo"                          # Nome do arquivo
-        diretorio=input("Insira o diretorio onde o arquivo .geo deve ser salvo: ")    # Diretorio onde o arquivo sera colocado
-    
-        if not os.path.exists(diretorio):                                             # Vê se o diretorio existe, caso contrario, cria um
+        if not os.path.exists(diretorio):
             os.makedirs(diretorio)
-        
-        filepath = os.path.join(diretorio, filename)                                  # Cria o caminho completo para o arquivo
-        
+
+        filepath = os.path.join(diretorio, filename)
+
         with open(filepath, 'w') as geo_file:
-            
-            for i, (x, y, z) in enumerate(Coordenadas_nos):                           # Descreve os nós
+            for i, (x, y, z) in enumerate(self.nodes):
                 geo_file.write(f'Point({i + 1}) = {{{x}, {y}, {z}, 1.0}};\n')
-            
-            for i, (start, end) in enumerate(conexões_nos):                           # Cria as linhas(elementos)
+
+            for i, (start, end) in enumerate(self.elements):
                 geo_file.write(f'Line({i + 1}) = {{{start + 1}, {end + 1}}};\n')
-            
-                            
-            if len(conexões_nos) > 2:                                                 # Cria um loop de linha e uma superfície plana se tivermos mais de 2 conexões
-                line_loop_indices = ', '.join(str(i + 1) for i in range(len(conexões_nos)))
+
+            if len(self.elements) > 2:
+                line_loop_indices = ', '.join(str(i + 1) for i in range(len(elements)))
                 geo_file.write(f'Line Loop(1) = {{{line_loop_indices}}};\n')
                 geo_file.write('Plane Surface(1) = {1};\n')
-            
-            geo_file.write('Mesh.Algorithm = 6;\n')                                   # Configurações da malha
+
+            geo_file.write('Mesh.Algorithm = 6;\n')
             geo_file.write('Mesh.ElementOrder = 1;\n')
             geo_file.write('Mesh.Format = 1;\n')
-    
-        print(f'O arquivo foi salvo em : {filepath}, basta abrir o GMSH, e abrir o arquivo')
-    
-        return Mesh
+
+        print(f'O arquivo foi salvo em: {filepath}, basta abrir o GMSH, e abrir o arquivo')
+
+#Coordenadas dos nós (x, y, z)
+nodes = [
+    (0, 0, 0),
+    (0, 0.375, 0),
+    (0, 0.700, 0),
+    (1.500, 0.375, 0),
+    (1.500, 0, 0),
+    (1.500, 0.700, 0)
+]
+
+#Conectividade dos elementos (índices dos nós)
+elements = [
+    (0, 1),  # Elemento entre os nós 0 e 1
+    (1, 2),  # Elemento entre os nós 1 e 2
+    (4, 3),  # Elemento entre os nós 4 e 3
+    (3, 5),  # Elemento entre os nós 3 e 5
+    (1, 3)  # Elemento entre os nós 1 e 3
+]
+
+#Criar a estrutura e montar as matrizes de rigidez e massa globais
+#Dados: n = len(nodes), 
+#       m = 1500 kg, 
+#       rho = 7850 kg/m^3
+#       A = 0.225 m^2
+#       E = 210e9  # Módulo de elasticidade em Pa
+#       I = 8.33e-6  # Momento de inércia em m^4
+#       Ip = Id = 8.33e-6 kg.m^2
+F1 = np.array([1000, 2000, 3000, 4000, 5000])
+F2 = np.array([1000, 2000, 3000, 4000, 5000])
+T = np.array([1000, 2000, 3000, 4000, 5000])
+estrutura = Estrutura(elements, nodes, 1500, 8.33e-6, 8.33e-6)
+K_global, M_global = estrutura.matrizes_globais()
+
+#Gerar as matrizes de localização dos nós e de conectividade
+node_tags = list(range(len(nodes)))
+estrutura.node_loc_matrix(node_tags, nodes)
+estrutura.connect_matrix()
+
+#Gerar autovalores, autovetores e frequências naturais
+autovalores, autovetores, frequencias = estrutura.modal_analysis()
+
+torsao, deformacao_axial, flexao  = estrutura.shape_fun(F1,F2,T)
+# Plotando os resultados das deformações
+fig, axs = plt.subplots(3, 1, figsize=(12, 18))
 
 
+# Plot da Torção
+axs[0].plot(torsao, 'o-', label=[f'Elemento {x}' for x in range(5)])
+axs[0].set_title('Deformação por Torção')
+axs[0].set_xlabel('Elemento')
+axs[0].set_ylabel('Torção (rad)')
+axs[0].legend()
 
-    def EIGSolver(self):
-        """Description.
+# Plot da Deformação Axial
+axs[1].plot(deformacao_axial, 's-', label=[f'Elemento {x}' for x in range(5)])
+axs[1].set_title('Deformação Axial')
+axs[1].set_xlabel('Elemento')
+axs[1].set_ylabel('Deformação (m)')
+axs[1].legend()
 
-        Detailed description.
+# Plot da Flexão
+for i, v_val in enumerate(flexao):
+    axs[2].plot(v_val, label=f'Elemento {i}')
+axs[2].set_title('Deformação por Flexão')
+axs[2].set_xlabel('Posição ao longo do elemento')
+axs[2].set_ylabel('Flexão(m)')
+axs[2].legend()
 
-        Returns
-        -------
-        Bat : variable type
-            Description.
-
-        Examples
-        --------
-        >>> example
-        """
-        
-        return EIG
+plt.tight_layout()
+plt.show()
 
 
+print("\n Matriz de Rigidez Global")
+print(K_global)
 
-    def TIMESolver(self):
-        """Description.
+print("\n Matriz de Massa Global")
+print(M_global)
 
-        Detailed description.
+# Convertendo a matriz de conectividade para um array numpy
+connectivity_array = np.array(elements)
 
-        Returns
-        -------
-        Bat : variable type
-            Description.
+# Plotando o gráfico 3D da estrutura
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
 
-        Examples
-        --------
-        >>> example
-        """
-        
+# Adicionando os pontos
+for i, (x, y, z) in enumerate(nodes):
+    ax.scatter(x, y, z, color='b', s=100)
+    ax.text(x, y, z, f'  {i}', color='black', fontsize=12)
+
+# Adicionando as linhas de ligação entre os nós
+for node1, node2 in elements:
+    x = [nodes[node1][0], nodes[node2][0]]
+    y = [nodes[node1][1], nodes[node2][1]]
+    z = [nodes[node1][2], nodes[node2][2]]
+    ax.plot(x, y, z, marker='o')
+
+# Configurações adicionais
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Estrutura 3D')
+
+plt.show()
+
+
+#Exibindo as frequências naturais e modos de vibração da estrutura
+print("\n Frequências Naturais (ω) da estrutura montada por vigas:")
+print(frequencias)
+
+#Plotagem dos modos de vibração para a estrutura de vigas
+for mode_idx in range(len(autovalores)):
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_title(f'Modo {mode_idx + 1} Viga')
+
+    for i, (x, y, z) in enumerate(nodes):
+        ax.scatter(x, y, z, color='b', s=100)
+        ax.text(x, y, z, f'  {i}', color='black', fontsize=8)
+
+    for node1, node2 in elements:
+        x = [nodes[node1][0], nodes[node2][0]]
+        y = [nodes[node1][1], nodes[node2][1]]
+        z = [nodes[node1][2], nodes[node2][2]]
+        ax.plot(x, y, z, 'b--')
+
+    mode_shape = autovetores[:, mode_idx]
+    displacements = np.zeros((len(nodes), 3))
+
+    for j, (x, y, z) in enumerate(nodes):
+        if j > 0 and j < len(nodes) - 1:
+            displacements[j, 0] = mode_shape[2 * j]
+            displacements[j, 1] = mode_shape[2 * j + 1]
+            displacements[j, 2] = 0
+
+    deformed_nodes = np.array(nodes) + displacements
+
+    for node1, node2 in elements:
+        x = [deformed_nodes[node1][0], deformed_nodes[node2][0]]
+        y = [deformed_nodes[node1][1], deformed_nodes[node2][1]]
+        z = [deformed_nodes[node1][2], deformed_nodes[node2][2]]
+        ax.plot(x, y, z, 'r-')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.tight_layout()
+    plt.show()
+
+estrutura.Mesh()
         return TIME
 
 
