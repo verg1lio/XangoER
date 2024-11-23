d6c06c5f83a93ba1386f04c243b44b3a953ffd29
Iago Silva <iagodiasep4@gmail.com>
2024-11-23 12:02:58 -0300

    Código Powertrain 23/11/2024 Co-authored-by: Fabricio Tosta <fabriciotosta.xango@gmail.com> Co-authored-by: Alan Rodrigues <alanzo.xango@gmail.com> Co-authored-by: Nikolas Rueckmann <nkls99@gmail.com> Co-authored-by: Caio Mazza <caio.mpmazza@gmail.com> Co-authored-by: Igor Maia <igormaia.xango@gmail.com> Co-authored-by: João Marcos <joaomarcos.xango@gmail.com>

--- digitaltwin_powertrain.py
+++ digitaltwin_powertrain.py
@@ -8,7 +8,7 @@ __all__ = ["Motor", "Peso"]
 
 
 class Motor:
-    def __init__(self, rs, rr, ls, lr, mrs, jm, kf):
+    def __init__(self, rs, rr, ls, lr, mrs, jm, kf, q1, q2, q3, valor_mu):
         # Constants
         self.pi23 = 2 * np.pi / 3
         self.rq23 = np.sqrt(2 / 3)
@@ -21,13 +21,19 @@ class Motor:
         self.lr = lr  # Rotor inductance (henries)
         self.msr = mrs  # Mutual inductance between stator and rotor (henries)
         self.lso = 0.1 * self.ls  # Stator leakage inductance (henries)
-        
+        self.f_onda_p = 50 # Frequência da onda portadora do sinal PWM
+        self.f_onda_m = 5 # Frequência da onda modulante do sinal PWM
+        self.q1 = q1 # Chave de comutação do inversor
+        self.q2 = q2 # Chave de comutação do inversor
+        self.q3 = q3 # Chave de comutação do inversor
         self.jm = jm  # Moment of inertia (kg*m^2)
         self.kf = kf  # Friction coefficient (N*m*s)
         self.cte_tempo_mec = self.jm / self.kf  # Mechanical time constant (s)
         self.idt = 1 / (self.ls * self.lr - self.msr * self.msr)  # Inverse of the determinant
         self.p = 2  # Number of pole pairs
         self.amsr = self.p * self.idt * self.msr  # Constant for torque calculation
+        self.valor_mu = valor_mu  # Escalar
+
 
         # Simulation parameters
         self.h = 1.e-5  # Time step (s)
@@ -677,15 +683,177 @@ class Motor:
         plt.grid(True)
         plt.legend()
         plt.show()
+        
+    def chaves(self):
+        """ A configuração de chaves do inversor.
+
+
+        Determina a configuração das seis chaves que 
+        compõem o do inversor de frequência (q1, q2, q3, q4, q5, q6). 
+        Sendo q4, q5 e q6 os complementares de q1, q2 e q3, respectivamente.
+        
+        Returns:
+            Chave 1: Chave 1 Fechada (True) ou Aberta (False)
+            Chave 2: Chave 2 Fechada (True) ou Aberta (False)
+            Chave 3: Chave 3 Fechada (True) ou Aberta (False)
+            Chave 4: Chave 4 Fechada (True) ou Aberta (False)
+            Chave 5: Chave 5 Fechada (True) ou Aberta (False)
+            Chave 6: Chave 6 Fechada (True) ou Aberta (False)
+                   
+        Example
+        >>> motor = Motor(0.39, 1.41, 0.094, 0.094, 0.091, 0.04, 0.01, q1=1, q2=1, q3=0, valor_mu=1) 
+        >>> motor.chaves()
+       
+        
+        """
+        
+        if self.q1 == 1: chave_1 = True # Retorno chave 1 fechada
+        else: chave_1 = False # Retorno chave 1 aberta
+
+        if self.q2 == 1: chave_2 = True # Retorno chave 2 fechada
+        else: chave_2 = False # Retorno chave 2 aberta
+
+        if self.q3 == 1: chave_3 = True # Retorno chave 3 fechada
+        else: chave_3 = False # Retorno chave 3 aberta
+
+        q1_bar = 1 - self.q1
+        q2_bar = 1 - self.q2
+        q3_bar = 1 - self.q3
+            
+        if q1_bar == 1: chave_4 = True # Retorno chave 1_bar fechada
+        else: chave_4 = False # Retorno chave 1_bar aberta
+
+        if q2_bar == 1: chave_5 = True # Retorno chave 2_bar fechada
+        else: chave_5 = False # Retorno chave 2_bar aberta
+
+        if q3_bar == 1: chave_6 = True # Retorno chave 3_bar fechada
+        else: chave_6 = False # Retorno chave 3_bar aberta    
+
+        return print(f'A configuração de chaves do inversor é: C1={chave_1}, C2={chave_2}, C3={chave_3}, C4={chave_4}, C5={chave_5}, C6={chave_6}')
+       
+       
+    def controle_pwm(self):
+        """Sinal PWM para controle do 
+
+        Implementa o controle PWM (Modulação por Largura de Pulso) para o sistema. A função realiza o cálculo das tensões moduladas, das correntes e do sinal PWM, além de gerar gráficos para análise visual dos resultados.
+
+        Returns
+        Plots do gráfico do sinal PWM, onda triangular portadora, correntes, tensão de entrada e tensão modulada
+        None
+
+        Examples
+        --------
+        >>> motor = Motor(0.39, 1.41, 0.094, 0.094, 0.091, 0.04, 0.01)
+        >>> motor.controle_pwm()
+        # O código exibirá gráficos de tensões moduladas, sinais PWM e correntes.
+
+        Notes
+        -----
+        A função considera parâmetros do sistema previamente definidos, como tensão de alimentação, frequência da onda portadora, e fator de modulação.
+
+        """
+
+        # Define o passo de tempo do controlador
+        self.t_pwm = np.linspace(0, 2, 1000)
+
+        # Tensões de entrada
+        self.v1 = self.Vs * np.sin(2 * np.pi * self.t_pwm)
+        self.v2 = self.Vs * np.sin(2 * np.pi * self.t_pwm + 2 * np.pi / 3)
+        self.v3 = self.Vs * np.sin(2 * np.pi * self.t_pwm + 4 * np.pi / 3)
+
+        # Correntes
+        self.i1 = self.Vs * np.sin(2 * np.pi * self.t_pwm - np.pi / 2)
+        self.i2 = self.Vs * np.sin(2 * np.pi * self.t_pwm + 2 * np.pi / 3 - np.pi / 2)
+        self.i3 = self.Vs * np.sin(2 * np.pi * self.t_pwm + 4 * np.pi / 3 - np.pi / 2)
+
+        # Cálculo máximo e mínimo das tensões
+        self.vN0max_star = (self.Vs / 2) - np.maximum.reduce([self.v1, self.v2, self.v3])
+        self.vN0mim_star = -self.Vs / 2 - np.minimum.reduce([self.v1, self.v2, self.v3])
+
+        # Tensão homopolar
+        self.vN0_star = self.valor_mu * self.vN0max_star + (1 - self.valor_mu) * self.vN0mim_star
+
+        # Tensões moduladas
+        self.v10 = self.v1 + self.vN0_star
+        self.v20 = self.v2 + self.vN0_star
+        self.v30 = self.v3 + self.vN0_star
+
+        # Geração da onda portadora triangular:
+        periodo_port = 1 / self.f_onda_p
+        onda_port = 1 - 2 * np.abs((self.t_pwm % periodo_port) * self.f_onda_p - 0.5)
+
+        # Sinal PWM
+        PWM_signal = np.where(self.v10 >= onda_port, self.Vs, 0)
+
+        # Correntes
+        plt.figure(figsize=(8, 4))
+        plt.plot(self.t_pwm, self.i1, label='Current PWM 1 (A)')
+        plt.plot(self.t_pwm, self.i2, label='Current PWM 2 (A)')
+        plt.plot(self.t_pwm, self.i3, label='Current PWM 3 (A)')
+        plt.title('Currents PWM (A)')
+        plt.legend()
+        plt.xlabel('Time (s)')
+        plt.ylabel('Current PWM (A)')
+
+        # Gráficos dos resultados
+        plt.figure(figsize=(10, 8))
+
+        # Tensão modulada
+        plt.subplot(4, 1, 1)
+        plt.plot(self.t_pwm, self.v10, label='V10', color='black')
+        plt.title('Tensão modulada')
+        plt.xlabel('Tempo [s]')
+        plt.ylabel('Tensão [V]')
+        plt.legend()
+
+        # Onda triangular portadora
+        plt.subplot(4, 1, 2)
+        plt.plot(self.t_pwm, onda_port, label='Onda portadora')
+        plt.title('Sinal da onda portadora')
+        plt.xlabel('Tempo [s]')
+        plt.ylabel('Tensão [V]')
+
+        # Gráfico do sinal PWM puro
+        plt.subplot(4, 1, 3)
+        plt.step(self.t_pwm, PWM_signal, label='PWM', color='red')
+        plt.title('Sinal PWM para controle do torque')
+        plt.xlabel('Tempo [s]')
+        plt.ylabel('Tensão [V]')
+
+        # Onda de referência
+        plt.subplot(4, 1, 4)
+        plt.step(self.t_pwm, self.v1, label='Tensão 1', color='green')
+        plt.title('Tensão de referência')
+        plt.xlabel('Tempo [s]')
+        plt.ylabel('Tensão [V]')
+
+        plt.tight_layout()
+        plt.show()
+
+
+    
+    def exec_pwm(self):
+        self.mu_values = np.linspace(0, 10e-1, 1)  # Variação de mu
+        
+        if self.valor_mu != 1:
+            self.controle_pwm()  
+        else:
+            for mu in self.mu_values:
+                
+                self.valor_mu = mu  
+                self.controle_pwm()  
+
 
     def example():
-        motor = Motor(0.39, 1.41, 0.094, 0.094, 0.091, 0.04, 0.01)
+        motor = Motor(0.39, 1.41, 0.094, 0.094, 0.091, 0.04, 0.01, q1=1, q2=1, q3=0, valor_mu=1) # Varia o valor de mu entre 0 e 1
         motor.simulate()
         motor.plot_motor()
         motor.plot_bode()
         motor.plot_nyquist() 
         motor.print_state_space()
         motor.step_response()
+        motor.chaves()
+        motor.exec_pwm()
 
 
 class Peso:
@@ -722,4 +890,3 @@ class Peso:
 # exemplos
 Motor.example()
 Peso.example()
-
