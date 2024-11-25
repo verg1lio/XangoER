ebcb77559d8ab74e86bf422a0737fd73d1d67083
Kevin-Johnny <kevinmata.xango@gmail.com>
2024-11-25 16:51:04 -0300

    Registro digitaltwin_dynamics.py
    Registro de Kevin Johnny como autor do código

--- digitaltwin_dynamics.py
+++ digitaltwin_dynamics.py
@@ -2285,4 +2285,4 @@ BrakeSystem.brake_system_example()
 Kinematics.kinematics_example()
 Tire.tire_example()
 
-#Autores do código: João Victor Dias França; Marco Affonso de Carvalho Santos; Catarina Eduarda Santos Gomes; Lucca Matheus Silva Macêdo; Ruan Felipe França Lutigards;
+#Autores do código: João Victor Dias França; Marco Affonso de Carvalho Santos; Catarina Eduarda Santos Gomes; Lucca Matheus Silva Macêdo; Ruan Felipe França Lutigards; Kevin Johnny Silva da Mata; 
